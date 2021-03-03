e076615a05abb493165bdf1c2a403c15e14582d9
JackGreene39 <jackmgreene39@gmail.com>
2021-03-03 16:59:37 -0600

    Pulling html from hmdb

--- Components/HomeScreen.js
+++ Components/HomeScreen.js
@@ -1,18 +1,39 @@
 import * as React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { Button, StyleSheet, View, Text } from 'react-native';
+import { loadNearMePage } from '../backend/WebScraping';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen( { navigation } ) {
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to AutoNarrator!</Text>
-			<Text>This is the Home Page.</Text>
-    </View>
-
-			
-
-  );
+class HomeScreen extends React.Component {
+	constructor(props) {
+		super(props);
+		this.loadWebData = this.loadWebData.bind(this);
+		this.state = {
+			webText: 'Testing'
+		}	
+	}
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text>Welcome to AutoNarrator!</Text>
+				<Text>{this.state.webText}</Text>
+				<Button
+					onPress={this.loadWebData}
+					title="Load Page"
+				/>
+			</View>
+		);
+	}
+	
+	async loadWebData() {
+		const cheerio = require('cheerio');
+		const searchUrl = 'https://www.hmdb.org/nearbylist.asp?nearby=yes&Latitude=30.6155318&Longitude=-96.3333562&submit=Show+List';
+		const response = await fetch(searchUrl);
+		
+		const htmlString = await response.text();
+		const cheerioResult = cheerio.load(htmlString);
+		this.setState({webText: htmlString}); 
+	}
 }
 
 const styles = StyleSheet.create({
