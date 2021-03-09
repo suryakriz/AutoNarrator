3689562548b94a10d6da44f49f48a092899df806
Jonathan Yang <jhy434@tamu.edu>
2021-03-09 15:46:17 -0600

    Update SettingsScreen.js

--- Components/SettingsScreen.js
+++ Components/SettingsScreen.js
@@ -1,17 +1,60 @@
 import React, { Component } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import Slider from "@react-native-community/slider";
+import DropDownPicker from "react-native-dropdown-picker";
+import * as Speech from "expo-speech";
+
+async function ttsList() {
+  try {
+    let r1 = await Speech.getAvailableVoicesAsync();
+    var returnList = [];
+    for (var i = 0; i < r1.length; i++) {
+      var loc = r1[i].identifier.indexOf("en");
+      if (loc != -1) {
+        returnList.push({
+          label: r1[i].identifier,
+          value: r1[i].identifier,
+        });
+      }
+    }
+    //console.log(returnList);
+    return returnList;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
+
+function DropdownItem(label) {
+  this.label = label;
+}
 
 export default class SettingsScreen extends Component {
   constructor() {
     super();
-    var voice = "";
+    var voiceList = [
+      {
+        label: "en-GB-language",
+        value: "en-GB-language",
+      },
+    ];
     this.state = {
       rate: 1.0,
       pitch: 1.0,
+      voice: "en-GB-language",
+      vList: voiceList,
     };
+    ttsList()
+      .then((result) => {
+        this.voiceList = result;
+        this.setState({
+          vList: this.voiceList,
+        });
+      })
+      .catch((err) => {
+        console.log("error");
+      });
   }
-  static defaultProps = {};
 
   render() {
     return (
@@ -27,6 +70,32 @@ export default class SettingsScreen extends Component {
           minimumValue={0}
           maximumValue={2}
           value={this.state.rate}
+        />
+        <Text style={styles.text}>
+          {this.state.pitch && +this.state.pitch.toFixed(3)}
+        </Text>
+        <Slider
+          step={0.5}
+          style={styles.slider}
+          onValueChange={(pitch) => this.setState({ pitch: pitch })}
+          minimumValue={0}
+          maximumValue={2}
+          value={this.state.pitch}
+        />
+        <DropDownPicker
+          items={this.state.vList}
+          defaultValue={this.state.voice}
+          containerStyle={{ height: 40, width: "50%" }}
+          style={{ backgroundColor: "#fafafa" }}
+          itemStyle={{
+            justifyContent: "flex-start",
+          }}
+          dropDownStyle={{ backgroundColor: "#fafafa" }}
+          onChangeItem={(item) =>
+            this.setState({
+              voice: item.value,
+            })
+          }
         />
       </View>
     );
