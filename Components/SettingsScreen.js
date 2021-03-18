e41a400a575d0fbda7ac93b540ce7dc9f69e9607
lauren-ruffner11 <lauren.ruffner11@tamu.edu>
2021-03-18 00:49:36 -0500

    Worked on the Voice Settings option on the settings screen

--- Components/SettingsScreen.js
+++ Components/SettingsScreen.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { Button, StyleSheet, View, Text } from "react-native";
 import Slider from "@react-native-community/slider";
 import DropDownPicker from "react-native-dropdown-picker";
 import * as Speech from "expo-speech";
 import Icon from 'react-native-ico-flags';
+import Icon2 from 'react-native-ico-ui-interface';
+import Icon3 from 'react-native-ico-essential';
 
 
 function DropdownItem(label) {
@@ -59,52 +61,61 @@ export default class SettingsScreen extends Component {
     this.state = {
       rate: 1.0,
       pitch: 1.0,
+      label: "US - Female",
       voice: "en-us-x-sfg#female_3-local",
       vList: voiceList,
     };
   }
 
+
+  speaktest(){
+    var thingToSay = "This is the voice "+ (this.state.label) +" at the speed " + this.state.rate + ". If you do not like it feel free to change the settings.";
+    Speech.speak(thingToSay, {voice: this.state.voice, rate: this.state.rate});
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>English Voice:</Text>
-        <DropDownPicker
-          items={this.state.vList}
-          defaultValue={this.state.voice}
-          containerStyle={{ height: 40, width: "50%", marginBottom: 10 }}
-          style={{ backgroundColor: "#fafafa" }}
-          itemStyle={{
-            justifyContent: "flex-start",
-          }}
-          dropDownStyle={{ backgroundColor: "#fafafa" }}
-          onChangeItem={(item) =>
-            this.setState({
-              voice: item.value,
-            })
-          }
-        />
-        <Text>
-          Voice Rate of Speech: {this.state.rate && +this.state.rate.toFixed(3)}
-        </Text>
-        <Slider
-          step={0.5}
-          style={styles.slider}
-          onValueChange={(rate) => this.setState({ rate: rate })}
-          minimumValue={0}
-          maximumValue={2}
-          value={this.state.rate}
-        />
-        <Text>
-          Voice Pitch: {this.state.pitch && +this.state.pitch.toFixed(3)}
-        </Text>
-        <Slider
-          step={0.5}
-          style={styles.slider}
-          onValueChange={(pitch) => this.setState({ pitch: pitch })}
-          minimumValue={0}
-          maximumValue={2}
-          value={this.state.pitch}
-        />
+        <View style={styles.header}>
+          <Icon2 color= "#ffffff" name="volume"/>
+          <Text style={{color: "#ffffff"}}> Voice Settings </Text>
+        </View>
+        <View style={styles.settings}>
+          <Text>English Voice:</Text>
+          <DropDownPicker
+            items={this.state.vList}
+            defaultValue={this.state.voice}
+            containerStyle={{ height: 40, width: "60%", marginBottom: 10, marginTop: 10 }}
+            style={{ backgroundColor: "#fafafa"}}
+            itemStyle={{
+              justifyContent: "flex-start",
+            }}
+            dropDownStyle={{ backgroundColor: "#fafafa",  elevation: 999  }}
+            onChangeItem={(item) =>
+              this.setState({
+                voice: item.value,
+                label: item.label,
+              })
+            }
+          />
+        </View>
+        
+        <View style={styles.settings}>
+          <Text>
+            Voice Speed: {this.state.rate && +this.state.rate.toFixed(3)}
+          </Text>
+          <Slider
+            step={0.5}
+            style={styles.slider}
+            onValueChange={(rate) => this.setState({ rate: rate })}
+            minimumValue={0.5}
+            maximumValue={3.5}
+            value={this.state.rate}
+          />
+        </View>
+        <Button 
+          title= "Click Here to Test Voice" 
+          onPress= {() => this.speaktest()}/>
       </View>
     );
   }
@@ -118,15 +129,33 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   slider: {
-    width: 300,
+    width: "65%",
     opacity: 1,
     height: 50,
     marginTop: 0,
   },
   text: {
-    fontSize: 14,
+    fontSize: 24,
     textAlign: "center",
     fontWeight: "500",
     margin: 10,
+  },
+  header: {
+    backgroundColor: "#214988",
+    fontWeight: "bold",
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    width: "90%",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    borderRadius:10,
+    
+  },
+  settings: {
+    width: "85%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
 });
