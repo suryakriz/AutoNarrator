9f48b95d94070c703d547c4df4b79aecb39ccef8
Jonathan Yang <jhy434@tamu.edu>
2021-03-29 14:35:42 -0500

    Update HomeScreen.js

--- Components/HomeScreen.js
+++ Components/HomeScreen.js
@@ -20,23 +20,6 @@ let customFonts = {
   "Quicksand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
 };
 
-async function ttsList() {
-  try {
-    let r1 = await Speech.getAvailableVoicesAsync();
-    var returnList = [];
-    for (var i = 0; i < r1.length; i++) {
-      var loc = r1[i].identifier.indexOf("en");
-      if (loc != -1) {
-        returnList.push(r1[i].identifier);
-      }
-    }
-    return returnList;
-  } catch (e) {
-    console.log(e);
-    throw e;
-  }
-}
-
 class HomeScreen extends React.Component {
   constructor(props) {
     super(props);
@@ -52,6 +35,10 @@ class HomeScreen extends React.Component {
       lat: null,
       long: null,
       errormsg: "",
+      inProgress: false,
+      intervalFunc: null,
+      intervalSet: false,
+      locationList: [],
     };
   }
 
@@ -61,6 +48,7 @@ class HomeScreen extends React.Component {
       this.setState({
         errorMessage: "Permission to access location was denied",
       });
+      return false;
     }
 
     let location = await Location.getCurrentPositionAsync({
@@ -74,6 +62,7 @@ class HomeScreen extends React.Component {
       lat: latitude,
       long: longitude,
     });
+    return true;
   };
   //FONT STUFF
   state = {
@@ -87,54 +76,88 @@ class HomeScreen extends React.Component {
     this._loadFontsAsync();
   }
 
-  async loadWebData() {
-    if (this.state.index == 0) {
-      //notVisited
-      var notVisited = true;
-      //put together the longitude and latitude of a current location
-      this.getLocationAsync();
-      //var longitude = this.state.long;
-      //var latitude = this.state.lat;
+  async loadData() {
+    console.log("Running Load Data");
+    if (this.state.index == 1) {
+      console.log("Speaking State");
+      const start = () => {
+        this.setState({ inProgress: true });
+      };
+      const complete = () => {
+        this.state.inProgress && this.setState({ inProgress: false });
+      };
 
-      var longitude = -96;
-      var latitude = 30;
+      var have_location = await this.getLocationAsync();
+      var longitude = this.state.long;
+      var latitude = this.state.lat;
+      //var longitude = -96;
+      //var latitude = 30;
 
-      //pull the list of locations from the longitude and latitiude
-      const cheerio = require("cheerio");
-      const searchUrl =
-        "https://www.hmdb.org/nearbylist.asp?nearby=yes&Latitude=" +
-        latitude +
-        "&Longitude=" +
-        longitude +
-        "&submit=Show+List";
-      var response = await fetch(searchUrl);
-      var htmlString = await response.text();
-      const listOfLocations = cheerio.load(htmlString)("a:even", "li");
-      for (var i = 0; i < listOfLocations.length; i++) {
-        console.log(listOfLocations.eq(i).text()); // logs individual sections
-        if (notVisited) {
-          var location = listOfLocations.eq(i);
-          var locationUrl =
-            "https://www.hmdb.org/" + listOfLocations.eq(i).attr("href"); //website of the
-          response = await fetch(locationUrl);
-          htmlString = await response.text();
-          //INFORMATION TO BE READ BY THE READER
-          var landmarkInfo = cheerio.load(htmlString)("#inscription1").text();
-          this.props.dispatch(VisitedListAdd(landmarkInfo));
-          Speech.speak(landmarkInfo, {
-            voice: this.props.voice,
-            rate: this.props.speed,
-          });
-          let promise = Speech.isSpeakingAsync();
-          let result = await promise;
-          console.log("Hi");
-          //return landmarkInfo;
+      if (have_location) {
+        const cheerio = require("cheerio");
+        const searchUrl =
+          "https://www.hmdb.org/nearbylist.asp?nearby=yes&Latitude=" +
+          latitude +
+          "&Longitude=" +
+          longitude +
+          "&submit=Show+List";
+        var response = await fetch(searchUrl);
+        var htmlString = await response.text();
+        const listOfLocations = cheerio.load(htmlString)("a:even", "li");
+        var count = 0;
+
+        while (count < listOfLocations.length) {
+          if (!this.state.inProgress) {
+            console.log("Not Speaking");
+            var location = listOfLocations.eq(count);
+            var locationUrl = "https://www.hmdb.org/" + location.attr("href"); //website of the
+
+            if (
+              this.state.locationList.includes(location.attr("href")) == false
+            ) {
+              console.log("Not Visited Location");
+              response = await fetch(locationUrl);
+              htmlString = await response.text();
+              this.setState((state) => {
+                const locationList = state.locationList.concat(
+                  location.attr("href")
+                );
+
+                return {
+                  locationList,
+                  value: "",
+                };
+              });
+              var landmarkInfo = cheerio
+                .load(htmlString)("#inscription1")
+                .text();
+              this.props.dispatch(VisitedListAdd(landmarkInfo));
+              Speech.speak(landmarkInfo, {
+                voice: this.props.voice,
+                rate: this.props.speed,
+                onStart: start,
+                onDone: complete,
+              });
+              return;
+            } else {
+              console.log("Visited Location");
+              count = count + 1;
+            }
+          } else {
+            console.log("Currently Speaking");
+            return;
+          }
         }
       }
     } else {
+      console.log("Stop Speaking");
       Speech.stop();
+      this.setState({ inProgress: false });
+      return;
     }
   }
+
+  loadWebDataOnInterval() {}
 
   //SWITCHING BUTTONS
   OnButtonPress = () => {
@@ -156,6 +179,31 @@ class HomeScreen extends React.Component {
         bottomText: "Press Here To Stop Drive",
       });
     }
+    if (this.state.intervalSet == false) {
+      console.log("Interval func not set therefor setting");
+      this.state.intervalSet = true;
+      this.state.intervalFunc = setInterval(() => {
+        console.log("interval");
+        if (this.state.index == 0) {
+          /*this.state.intervalSet = false;
+          console.log("clearInterval");
+          clearInterval(this.state.intervalFunc);
+          this.state.intervalFunc = null;*/
+          console.log("Exiting Interval");
+          return;
+        } else {
+          this.loadData();
+        }
+      }, 10000);
+    } else {
+      this.state.intervalSet = false;
+      console.log("clearInterval");
+      clearInterval(this.state.intervalFunc);
+      this.state.intervalFunc = null;
+      console.log("Should be clearing speak");
+      Speech.stop();
+      this.setState({ inProgress: false });
+    }
   };
 
   render() {
@@ -171,7 +219,6 @@ class HomeScreen extends React.Component {
           <View>
             <TouchableOpacity
               onPress={() => {
-                this.loadWebData();
                 this.OnButtonPress();
               }}
               style={styles.buttonContainer}
