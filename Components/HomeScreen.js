b14b395194c56c2f4aca61a90ec12dc68cdd4228
lauren-ruffner11 <lauren.ruffner11@tamu.edu>
2021-03-30 14:57:01 -0500

    Added after drive popup and time calculations

--- Components/HomeScreen.js
+++ Components/HomeScreen.js
@@ -6,7 +6,9 @@ import {
   Button,
   FlatList,
   Text,
+  Modal,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
@@ -16,6 +18,11 @@ import { VisitedListAdd } from "../Redux/VisitedSlice";
 import { PastTripsAdd, AddLandmarkToTrip } from "../Redux/PastTripsSlice";
 import * as Location from "expo-location";
 import * as Permissions from "expo-permissions";
+import AwesomeButtonBlue from "react-native-really-awesome-button/src/themes/blue";
+import Icon from 'react-native-ico-miscellaneous';
+import Icon2 from 'react-native-ico-basic';
+import { mdiOrnament } from "@mdi/js";
+import moment from 'moment';
 
 let customFonts = {
   "Quicksand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
@@ -40,7 +47,18 @@ class HomeScreen extends React.Component {
       intervalFunc: null,
       intervalSet: false,
       locationList: props.visited,
+      isVisible: false,
+      date: "",
+      starttime: "",
+      starttime2: null,
+      endtime:"",
+      endtime2: null,
+      length: null,
     };
+  }
+
+  displayModal(show){
+    this.setState({isVisible: show})
   }
 
   getLocationAsync = async () => {
@@ -65,6 +83,7 @@ class HomeScreen extends React.Component {
     });
     return true;
   };
+
   //FONT STUFF
   state = {
     fontsLoaded: false,
@@ -157,6 +176,7 @@ class HomeScreen extends React.Component {
 
   //SWITCHING BUTTONS
   OnButtonPress = () => {
+    console.log("Current Index: " + this.state.index)
     if (this.imageBool) {
       this.imageBool = false;
       this.setState({
@@ -176,6 +196,11 @@ class HomeScreen extends React.Component {
       });
     }
     if (this.state.intervalSet == false) {
+      this.setState({
+        date: moment().format('MM/DD/YYYY'),
+        starttime: moment().format('hh:mm a'),
+        starttime2: Date.now(),
+      })
       console.log("Interval func not set therefore setting");
       this.state.intervalSet = true;
       this.state.intervalFunc = setInterval(() => {
@@ -196,12 +221,65 @@ class HomeScreen extends React.Component {
       Speech.stop();
       this.setState({ inProgress: false });
     }
+    console.log("New Index: " + this.state.index)
+    if (this.state.index == 1){
+      this.setState({
+        endtime: moment().format('hh:mm a'),
+      })
+      var duration = Date.now() - this.state.starttime2;
+
+      var minutes = Math.floor((duration / (1000 * 60)) % 60);
+      var hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+      hours = (hours < 10) ? "0" + hours : hours;
+      minutes = (minutes < 10) ? "0" + minutes : minutes;
+      this.setState({
+        length: hours + " hours and " + minutes + " minutes",
+      })
+      this.displayModal(true);
+    }
   };
 
   render() {
     if (this.state.fontsLoaded) {
       return (
         <View style={styles.container}>
+          <Modal
+            animationType = {"slide"}
+            transparent={false}
+            visible={this.state.isVisible}
+            onRequestClose={() => {
+              Alert.alert('Modal has now been closed.');
+            }}>
+              <View style={{height: "95%"}}>
+                <Text style = { styles.panelHeader }> After Drive Summary! </Text>
+                <ScrollView style={styles.scroll}>
+                  <View style={styles.header}>
+                    <Icon color="#ffffff" height={20} name="car" />
+                    <Text style={{ marginLeft:"3%" , color: "#ffffff", fontFamily: "Quicksand-Regular"}}> Drive Information </Text>
+                  </View>
+                  <Text style={styles.paneltext}>{'\u2022 Date: '} {this.state.date}</Text>
+                  <Text style={styles.paneltext}>{'\u2022 Start Time: '} {this.state.starttime}</Text>
+                  <Text style={styles.paneltext}>{'\u2022 End Time: '} {this.state.endtime}</Text>
+                  <Text style={styles.paneltext}>{'\u2022 Trip Length: '} {this.state.length}</Text>
+                  <View style={styles.header}>
+                    <Icon2 color="#ffffff" height={20} name="achievement" />
+                    <Text style={{ marginLeft:"3%" , color: "#ffffff", fontFamily: "Quicksand-Regular"}}> Landmark Report </Text>
+                  </View>
+                </ScrollView>
+                <View style={styles.button}>
+                  <AwesomeButtonBlue
+                    progress
+                    type="primary"
+                    height={40}
+                    onPress={() => {
+                      this.displayModal(!this.state.isVisible);}}
+                  > 
+                    <Text style = { styles.textButton }>Close Panel</Text>
+                  </AwesomeButtonBlue>
+                </View>
+              </View>
+          </Modal>
           <View>
             <Text style={styles.HomeTextTop}>{this.state.topText}</Text>
           </View>
@@ -268,5 +346,52 @@ const styles = StyleSheet.create({
     fontSize: 24,
     textAlign: "center",
     fontFamily: "Quicksand-Regular",
+  },
+  panelHeader: {
+    fontSize: 32,
+    fontFamily: "Quicksand-Regular",
+    marginTop: 10,
+    marginBottom: 10,
+    textAlign: "center",
+  },
+  paneltext: {
+    fontSize: 18,
+    fontFamily: "Quicksand-Regular",
+    marginLeft: 20,
+    marginTop: 5,
+  },
+  textButton: {
+    fontSize: 18,
+    fontFamily: "Quicksand-Regular",
+    color: "#fff",
+    padding: 5,
+  },
+  closeText: {
+    fontSize: 24,
+    color: '#00479e',
+    textAlign: 'center',
+    fontFamily: "Quicksand-Regular",
+  },
+  button: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  scroll: {
+    height: "70%",
+    width: "100%",
+  },
+  header: {
+    backgroundColor: "#214988",
+    fontWeight: "bold",
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 10,
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    borderRadius: 10,
   },
 });
