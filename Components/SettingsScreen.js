3faa46e2a806a27af852ebb4a4d99d6d08f3d9ca
lauren-ruffner11 <lauren.ruffner11@tamu.edu>
2021-03-31 17:07:36 -0500

    Revert "Merge branch 'master' into jack_branch"
    This reverts commit 68ac133cf368fd2114a75a33421871828d213d89, reversing
    changes made to 32f4a0a5d5041d13165f66bcdf40428f4842e447.

--- Components/SettingsScreen.js
+++ Components/SettingsScreen.js
@@ -3,12 +3,13 @@ import { Button, StyleSheet, View, Text, Alert } from "react-native";
 import Slider from "@react-native-community/slider";
 import DropDownPicker from "react-native-dropdown-picker";
 import * as Speech from "expo-speech";
-import { SetVoice, SetSpeed, SetTimeBetween } from "../Redux/SettingsSlice";
-import { connect, useDispatch } from "react-redux";
-import Icon from "react-native-ico-flags";
-import Icon2 from "react-native-ico-ui-interface";
-import Icon3 from "react-native-ico-essential";
+import { SetVoice, SetSpeed, SetTimeBetween } from '../Redux/SettingsSlice'
+import { connect, useDispatch } from 'react-redux'
+import Icon from 'react-native-ico-flags';
+import Icon2 from 'react-native-ico-ui-interface';
+import Icon3 from 'react-native-ico-essential';
 import AwesomeButtonBlue from "react-native-really-awesome-button/src/themes/blue";
+
 
 function DropdownItem(label) {
   this.label = label;
@@ -21,81 +22,81 @@ class SettingsScreen extends Component {
       {
         label: "US - Male",
         value: "en-us-x-sfg#male_3-local",
-        icon: () => <Icon name="united-states-of-america" size={18} />,
+        icon: () => <Icon name="united-states-of-america" size={18} />
       },
       {
         label: "US - Female",
         value: "en-us-x-sfg#female_3-local",
-        icon: () => <Icon name="united-states-of-america" size={18} />,
+        icon: () => <Icon name="united-states-of-america" size={18}/>
       },
       {
         label: "UK - Male",
         value: "en-gb-x-rjs-network",
-        icon: () => <Icon name="united-kingdom" size={18} />,
+        icon: () => <Icon name="united-kingdom" size={18}/>
       },
       {
         label: "UK - Female",
         value: "en-gb-x-fis-local",
-        icon: () => <Icon name="united-kingdom" size={18} />,
+        icon: () => <Icon name="united-kingdom" size={18} />
       },
       {
         label: "AUS - Male",
         value: "en-au-x-aub-network",
-        icon: () => <Icon name="australia" size={18} />,
+        icon: () => <Icon name="australia" size={18}/>
       },
       {
         label: "AUS - Female",
         value: "en-au-x-auc-network",
-        icon: () => <Icon name="australia" size={18} />,
+        icon: () => <Icon name="australia" size={18} />
       },
       {
         label: "IND - Male",
         value: "en-in-x-cxx#male_1-local",
-        icon: () => <Icon name="india" size={18} />,
+        icon: () => <Icon name="india" size={18} />
       },
       {
         label: "IND - Female",
         value: "en-in-x-cxx-network",
-        icon: () => <Icon name="india" size={18} />,
+        icon: () => <Icon name="india" size={18} />
       },
     ];
 
     var timeList = [
       {
         label: "30 seconds",
-        value: 30000,
+        value: 30
       },
       {
         label: "1 minute",
-        value: 60000,
+        value: 60
       },
       {
         label: "5 minutes",
-        value: 300000,
+        value: 300
       },
       {
         label: "10 minutes",
-        value: 600000,
+        value: 600
       },
       {
         label: "15 minutes",
-        value: 900000,
+        value: 900
       },
       {
         label: "20 minutes",
-        value: 1200000,
+        value: 1200
       },
       {
         label: "30 minutes",
-        value: 1800000,
+        value: 1800
       },
       {
         label: "45 minutes",
-        value: 2700000,
+        value: 2700
       },
       {
         label: "1 hour",
-        value: 3600000,
+        value: 3600
       },
     ];
     this.state = {
@@ -108,20 +109,12 @@ class SettingsScreen extends Component {
       time: this.props.time,
     };
 
-    this.ChangeVoice = this.ChangeVoice.bind(this);
+    this.ChangeVoice = this.ChangeVoice.bind(this)
   }
 
-  async speaktest() {
-    var thingToSay =
-      "This is the voice " +
-      this.state.label +
-      " at the speed " +
-      this.state.rate +
-      ". If you do not like it feel free to change the settings.";
-    Speech.speak(thingToSay, {
-      voice: this.state.voice,
-      rate: this.state.rate,
-    });
+  async speaktest(){
+    var thingToSay = "This is the voice "+ (this.state.label) +" at the speed " + this.state.rate + ". If you do not like it feel free to change the settings.";
+    Speech.speak(thingToSay, {voice: this.state.voice, rate: this.state.rate});
   }
 
   createTwoButtonAlert = () =>
@@ -132,9 +125,9 @@ class SettingsScreen extends Component {
         {
           text: "Cancel",
           onPress: () => console.log("Cancel Pressed"), //PUT FUNCTION TO DELETE DATA
-          style: "cancel",
+          style: "cancel"
         },
-        { text: "Confirm", onPress: () => console.log("Confirm Pressed") },
+        { text: "Confirm", onPress: () => console.log("Confirm Pressed") }
       ]
     );
 
@@ -142,27 +135,23 @@ class SettingsScreen extends Component {
     return (
       <View style={styles.container}>
         <View style={styles.header}>
-          <Icon3 color="#ffffff" height={20} name="speaker-2" />
-          <Text style={{ color: "#ffffff" }}> Voice Settings </Text>
+          <Icon3 color= "#ffffff" height= {20} name="speaker-2"/>
+          <Text style={{color: "#ffffff"}}> Voice Settings </Text>
         </View>
         <View style={styles.settings}>
           <Text>English Voice Accent:</Text>
           <DropDownPicker
             items={this.state.vList}
             defaultValue={this.state.voice}
-            containerStyle={{
-              height: 40,
-              width: "48%",
-              marginBottom: 10,
-              marginTop: 10,
-              marginRight: "4%",
-            }}
-            style={{ backgroundColor: "#fafafa" }}
+            containerStyle={{ height: 40, width: "48%", marginBottom: 10, marginTop: 10, marginRight: "4%"}}
+            style={{ backgroundColor: "#fafafa"}}
             itemStyle={{
               justifyContent: "flex-start",
             }}
-            dropDownStyle={{ backgroundColor: "#fafafa", elevation: 999 }}
-            onChangeItem={(item) => this.ChangeVoice(item)}
+            dropDownStyle={{ backgroundColor: "#fafafa",  elevation: 999  }}
+            onChangeItem={(item) =>
+              this.ChangeVoice(item)
+            }
           />
         </View>
         <View style={styles.settings}>
@@ -181,48 +170,44 @@ class SettingsScreen extends Component {
         <View style={styles.settings}>
           <AwesomeButtonBlue
             progress
-            type="primary"
-            height={40}
-            onPress={(next) => {
-              this.speaktest();
-              next();
+            type= "primary"
+            height= {40}
+            onPress={next => {
+            this.speaktest();
+            next();
             }}
           >
             Click Here to Test Voice
           </AwesomeButtonBlue>
         </View>
         <View style={styles.header}>
-          <Icon3 color="#ffffff" name="settings" />
-          <Text style={{ color: "#ffffff" }}> Other Settings </Text>
+          <Icon3 color= "#ffffff" name="settings"/>
+          <Text style={{color: "#ffffff"}}> Other Settings </Text>
         </View>
         <View style={styles.settings}>
           <Text>Time Between Landmarks: </Text>
           <DropDownPicker
             items={this.state.tList}
             defaultValue={this.state.time}
-            containerStyle={{
-              height: 40,
-              width: "48%",
-              marginBottom: 10,
-              marginTop: 10,
-              marginRight: "4%",
-            }}
-            style={{ backgroundColor: "#fafafa" }}
+            containerStyle={{ height: 40, width: "48%", marginBottom: 10, marginTop: 10, marginRight: "4%"}}
+            style={{ backgroundColor: "#fafafa"}}
             itemStyle={{
               justifyContent: "flex-start",
             }}
-            dropDownStyle={{ backgroundColor: "#fafafa", elevation: 999 }}
-            onChangeItem={(item) => this.ChangeTimeBetween(item)}
+            dropDownStyle={{ backgroundColor: "#fafafa",  elevation: 999  }}
+            onChangeItem={(item) =>
+              this.ChangeTimeBetween(item)
+            }
           />
         </View>
         <View style={styles.settings}>
           <AwesomeButtonBlue
             progress
-            type="primary"
-            height={40}
-            onPress={(next) => {
-              this.createTwoButtonAlert();
-              next();
+            type= "primary"
+            height= {40}
+            onPress={next => {
+            this.createTwoButtonAlert();
+            next();
             }}
           >
             Click Here to Clear Data
@@ -236,35 +221,34 @@ class SettingsScreen extends Component {
     this.setState({
       voice: newVoice.value,
       label: newVoice.label,
-    });
-    this.props.dispatch(
-      SetVoice({ voice: newVoice.value, label: newVoice.label })
-    );
+    })
+    this.props.dispatch(SetVoice({voice: newVoice.value, label: newVoice.label}))
   }
 
   ChangeSpeed(rate) {
-    this.setState({ rate: rate });
-    this.props.dispatch(SetSpeed(rate));
+    this.setState({ rate: rate })
+    this.props.dispatch(SetSpeed(rate))
   }
 
   ChangeTimeBetween(rate) {
     this.setState({
-      time: rate.value,
-    });
+      time: rate.value
+    })
     this.props.dispatch(SetTimeBetween(rate.value));
   }
+  
 }
 
 function mapStateToProps(state) {
-  return {
-    voice: state.settings.voiceName,
-    rate: state.settings.talkSpeed,
-    time: state.settings.timeBetween,
+	return {
+	  voice: state.settings.voiceName,
+	  rate: state.settings.talkSpeed,
+	  time: state.settings.timeBetween,
     label: state.settings.voiceLabel,
-  };
+	}
 }
-
-export default connect(mapStateToProps)(SettingsScreen);
+  
+export default connect(mapStateToProps)(SettingsScreen)
 
 const styles = StyleSheet.create({
   container: {
@@ -295,7 +279,8 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "flex-start",
     alignItems: "center",
-    borderRadius: 10,
+    borderRadius:10,
+    
   },
   settings: {
     width: "90%",
--- Redux/SettingsSlice.js
+++ Redux/SettingsSlice.js
@@ -1,28 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-  talkSpeed: 1,
-  voiceName: "en-au-x-auc-network",
-  voiceLabel: "AUS - Female",
-  timeBetween: 30000,
-};
+    talkSpeed: 1,
+    voiceName: "en-au-x-auc-network",
+    voiceLabel: "AUS - Female",
+    timeBetween: 30
+}
 
 const SettingsSlice = createSlice({
-  name: "SettingsSlice",
-  initialState,
-  reducers: {
-    SetVoice(state, action) {
-      state.voiceName = action.payload.voice;
-      state.voiceLabel = action.payload.label;
-    },
-    SetSpeed(state, action) {
-      state.talkSpeed = action.payload;
-    },
-    SetTimeBetween(state, action) {
-      state.timeBetween = action.payload;
-    },
-  },
-});
+    name: "SettingsSlice",
+    initialState,
+    reducers: {
+        SetVoice(state, action) {
+            state.voiceName = action.payload.voice
+            state.voiceLabel = action.payload.label
+        },
+        SetSpeed(state, action) {
+            state.talkSpeed = action.payload
+        },
+        SetTimeBetween(state, action) {
+            state.timeBetween = action.payload
+        },
+    }
+})
 
-export const { SetSpeed, SetVoice, SetTimeBetween } = SettingsSlice.actions;
-export default SettingsSlice.reducer;
+export const { SetSpeed, SetVoice, SetTimeBetween } = SettingsSlice.actions
+export default SettingsSlice.reducer
