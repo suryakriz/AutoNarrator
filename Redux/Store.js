25143ec0ef90af8471086585e0c13f4689500bc1
JackGreene39 <jackmgreene39@gmail.com>
2021-04-05 17:24:22 -0500

    Persisting redux store

--- Redux/Store.js
+++ Redux/Store.js
@@ -2,6 +2,10 @@ import { configureStore } from '@reduxjs/toolkit'
 import VisitedReducer from './VisitedSlice'
 import SettingsReducer from './SettingsSlice'
 import PastTripsReducer from './PastTripsSlice'
+import { loadState, saveState } from './LocalStorage'
+import throttle from 'lodash/throttle';
+
+const persistedState = loadState();
 
 const store = configureStore({ 
     reducer: {
@@ -9,6 +13,14 @@ const store = configureStore({
         settings: SettingsReducer,
         pastTrips: PastTripsReducer
     }
-})
+}, persistedState)
+
+store.subscribe(throttle(() => {
+    saveState({
+      visited: store.getState().visited,
+      settings: store.getState().settings,
+      pastTrips: store.getState().pastTrips,
+    });
+  }, 1000));
 
 export default store
